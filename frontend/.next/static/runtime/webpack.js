f7c272b9b0a047b178a0625bd2e322d367ee6fbf
Margarita Chernetski <lp@chernetski.com>
2018-11-28 16:35:22 -0600

    graphql yoga added

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4ebcf35a42eb23c016cf";
+/******/ 	var hotCurrentHash = "78955032aa2bc1290aa7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
